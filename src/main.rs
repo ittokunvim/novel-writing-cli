c0cfe56c5336e0f53ef7bfcddfc918f8699c049f
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-12-25 15:15:49 +0900

    feat: :sparkles: novel output to cli (#1)
    Issue: #1
    
    Close: #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = File::open("novel.txt").expect("404");
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("{contents}");
 }
